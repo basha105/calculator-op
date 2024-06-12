11c0494624bd680f6d57f6c81743c01b84b6fc0f
basha105 <brentbasha17@gmail.com>
2024-06-11 22:33:31 -0700

    Modfied

--- javascript.js
+++ javascript.js
@@ -34,16 +34,55 @@ function operate(n1, n2, op) {
     }
 }
 
-function alertFunction() {
-    alert("Hello");
-}
-
 let myDisplay = document.getElementById("display");
+myDisplay.textContent = "";
+
+let a;
+let b;
+let currentOperator;
+let lastEntryIsOperator = false;
+
+
+
 
 const numbers = document.querySelectorAll(".num");
 numbers.forEach((number) => {
     number.addEventListener("click", () => {
-        myDisplay.textContent = number.id;
-        
+        if (lastEntryIsOperator) {
+
+            if (typeof a === 'undefined') {
+                a = Number(myDisplay.textContent);
+            }
+            else if (typeof b === 'undefined') {
+                b = Number(myDisplay.textContent);
+            }
+
+            if (!typeof b === 'undefined') {
+                myDisplay
+                
+            }
+            
+
+            myDisplay.textContent = "" + String(number.id);
+
+            
+
+        }
+        else {
+            myDisplay.textContent = myDisplay.textContent + String(number.id);
+            
+        }
+        lastEntryIsOperator = false;
     });
 });
+
+
+const operators = document.querySelectorAll(".op");
+operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        currentOperator = operator.id;
+        lastEntryIsOperator = true;
+    });
+});
+
+const equals = document.querySelector("")
